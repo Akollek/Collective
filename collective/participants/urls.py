2b43b004e8fd43e815248943dcf2bcf4389e1eb6
AKollek <amiel.kollek@mail.mcgill.ca>
2014-09-13 03:50:26 -0400

    fixing up urls

--- collective/participants/urls.py
+++ collective/participants/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from participants import views
 
 urlpatterns = patterns('',
+    url(r'^text/', views.recieve_text, name='recieve_text'),
     url(r'^$', views.index, name='index'),
 )
--- collective/participants/views.py
+++ collective/participants/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from participants.models import Participant
+import twilio.twiml
 
 def index(request):
     return HttpResponse("Test")
 
 
-def create_user(request):
-
+def recieve_text(request):
+    textBody = request.GET['Body']
+    return HttpResponse("You sent the text: " + textBody)
