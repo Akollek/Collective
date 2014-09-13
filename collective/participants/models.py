47538e688a3adca632301c821702aabdcbbb0751
AKollek <amiel.kollek@mail.mcgill.ca>
2014-09-13 02:26:47 -0400

    Participant model

---
+++ collective/participants/models.py
@@ -0,0 +1,8 @@
+gfrom django.db import models
+
+class Participant(models.Model):
+    number   = models.CharField(max_length=20)
+    username = models.CharField(max_length=200)
+    joined   = models.DateTimeField('time joined')
+    score    = models.IntegerField(default=0)
+    played   = models.IntegerField(default=0)
